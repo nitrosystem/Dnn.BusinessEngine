8c344ad0bcb7b1553b3975150a932cf8e4b3afe2
hossein norouzi <norouzi.hosein@outlook.com>
2025-09-22 15:48:42 +0330

    Add ignored files

---
+++ Infrastructure/NitroSystem.Dnn.BusinessEngine.Common/Models/Shared/ErrorResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroSystem.Dnn.BusinessEngine.Shared.Models.Shared
+{
+    public class ErrorResult
+    {
+        public string ErrorMessage { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
